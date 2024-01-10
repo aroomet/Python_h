4c4252076e8c2bc9eed062863f19e9d6dfa3a670
RoometA <roomet.altmae@hkhk.edu.ee>
2024-01-10 15:32:45 +0200

    Create is.too4.py

---
+++ is.too4.py
@@ -0,0 +1,58 @@
+# Iseseisev töö
+#R.Altmäe
+#10.01.23
+
+kassa = 0
+fail = open("myndid.txt")
+for mynt in fail:
+    print(mynt)
+
+
+
+
+
+
+
+
+
+
+#---------------------
+kylalised = 3
+
+def tervitus(n):
+    print('Võõrustaja: "Tere!"')
+    print(f"Täna {n}. kord tervitada!")
+    print('Külaline: "Tere, suur tänu kutsumast!"')
+
+for i in range(kylalised):
+    tervitus(i+1)
+
+#---------------------
+def eelarve(kylalised):
+    summa = kylalised * 10 + 55
+    return summa
+palju = int(input("Mitu kutsutud: "))
+palju2 = int(input("Mitu tuleb: "))
+print(f"Maksimaalne eelarve: {eelarve(palju)}€")
+print(f"Minimaalne eelarve: {eelarve(palju2)}€")
+
+
+
+
+#---------------------
+def mahlapakkide_arv(kg):
+    pakid = round(kg * 0.4 / 3)
+    return pakid
+kogus = int(input("Õunte kogus: "))
+print(mahlapakkide_arv(kogus))
+
+
+#---------------------
+def banner(t):
+    print(t.upper())
+
+ask = int(input("Mitu korda: "))
+ask2 = input("Reklaamlause: ")
+
+for i in range(ask):
+    banner(ask2)
