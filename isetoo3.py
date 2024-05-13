984618ecc0c2e462eec86c3f5d75f8afc4d88fe1
buustujurger <145278950+aroomet@users.noreply.github.com>
2024-05-13 10:32:00 +0300

    Update isetoo3.py

--- isetoo3.py
+++ isetoo3.py
@@ -27,28 +27,36 @@ Temperatuurid - Programm peab tĆ¶Ć¶tlema Ć¼he aasta kĆµigi pĆ¤evade te
 	november -6 -7 -2 -7 -2 -4 0 -7 -8 -6 0 -9 -2 -3 -2 0 -8 -2 -5 -2 -5 -8 -10 0 -2 -9 -9 -7 -1
 	detsember -15 2 -11 -14 -15 -5 -5 -18 -18 -19 0 0 2 -7 -16 -7 -4 -1 -1 -16 -18 -10 -3 -19 -6 -16 -16 -8 -2 -18
 """
-temperatuurid = [
-    jaanuar := [-16, -12, -15, -20, 0, -1, -20, -2, -20, -14, -18, -8, 2, -1, -14, -7, -15, -17, -6, -17, -17, -7, 0, 3, -20, -17, -15, -8, -12, 3],
-    veebruar := [-9, -2, -7, 100, -16, -19, -19, -11, -16, -15, -9, -2, -16, -4, -20, -5, -6, -17, -5, 0, -16, 2, 0, -20, -16, -2, -18],
-    märts := [2, -9, -1, -3, -6, -2, 1, -2, -3, -9, -1, -4, 0, -6, -7, 1, 0, 2, -5, -10, 2, -7, -3, 2, -10, 2, -9, -8, -5, -2],
-    aprill := [-5, 0, 10, -9, 0, -9, -8, 6, -5, 3, -1, 4, 9, -1, 2, 0, 10, 0, 5, 0, -10, 0, 6, 3, -6, -2, -10, -8, -2],
-    mai := [12, 5, 8, -1, -2, 4, 10, -1, 7, 15, 7, 3, 6, 4, 10, 9, 13, 6, 14, 10, 14, 2, 6, 12, 15, 2, 14, 11, 9, 1],
-    juuni := [12, 5, 17, 6, 10, 14, 9, 7, 15, 23, 29, 11, 16, 18, 9, 25, 14, 8, 16, 22, 19, 22, 23, 18, 16, 16, 26, 24, 22],
-    juuli := [15, 8, 21, 28, 18, 13, 9, 9, 8, 6, 8, 12, 12, 29, 28, 20, 6, 9, 12, 8, 14, 18, 14, 13, 23, 6, 24, 24, 17, 20],
-    august := [7, 6, 5, 19, 18, 18, 17, 20, 15, 11, 7, 10, 13, 12, 20, 11, 10, 14, 18, 14, 24, 6, 17, 16, 6, 17, 5, 13, 11],
-    september := [21, 19, 21, 9, 13, 18, 6, 6, 20, 7, 25, 13, 8, 9, 14, 16, 19, 10, 7, 25, 7, 17, 16, 15, 17, 18, 15, 9, 19],
-    oktoober := [2, 2, 1, 5, -2, 5, 5, 2, 2, 2, 1, -2, 1, -2, 0, -2, 5, 4, 0, 1, -1, 2, 0, 2, 2, 2, -1, 1, 4, -1],
-    november := [-6, -7, -2, -7, -2, -4, 0, -7, -8, -6, 0, -9, -2, -3, -2, 0, -8, -2, -5, -2, -5, -8, -10, 0, -2, -9, -9, -7, -1],
-    detsember := [-15, 2, -11, -14, -15, -5, -5, -18, -18, -19, 0, 0, 2, -7, -16, -7, -4, -1, -1, -16, -18, -10, -3, -19, -6, -16, -16, -8, -2, -18]
-]
+def Temperatuurid():
+    kuud=["jaanuar","veebruar","märts","aprill","mai","juuni","juuli","august","september","oktoober","november","detsember"]
+    temp=[[-16,-12,-15,-20,0,-1,-20,-2,-20,-14,-18,-8,2,-1,-14,-7,-15,-17,-6,-17,-17,-7,0,3,-20,-17,-15,-8,-12,3],
+          [-9,-2,-7,1,-16,-19,-19,-11,-16,-15,-9,-2,-16,-4,-20,-5,-6,-17,-5,0,-16,2,0,-20,-16,-2,-18],
+          [2,-9,-1,-3,-6,-2,1,-2,-3,-9,-1,-4,0,-6,-7,1,0,2,-5,-10,2,-7,-3,2,-10,2,-9,-8,-5,-2],
+          [-5,0,10,-9,0,-9,-8,6,-5,3,-1,4,9,-1,2,0,10,0,5,0,-10,0,6,3,-6,-2,-10,-8,-2],
+          [12,5,8,-1,-2,4,10,-1,7,15,7,3,6,4,10,9,13,6,14,10,14,2,6,12,15,2,14,11,9,1],
+          [12,5,17,6,10,14,9,7,15,23,29,11,16,18,9,25,14,8,16,22,19,22,23,18,16,16,26,24,22],
+          [15,8,21,28,18,13,9,9,8,6,8,12,12,29,28,20,6,9,12,8,14,18,14,13,23,6,24,24,17,20],
+          [7,6,5,19,18,18,17,20,15,11,7,10,13,12,20,11,10,14,18,14,24,6,17,16,6,17,5,13,11],
+          [21,19,21,9,13,18,6,6,20,7,25,13,8,9,14,16,19,10,7,25,7,17,16,15,17,18,15,9,19],
+          [2,2,1,5,-2,5,5,2,2,2,1,-2,1,-2,0,-2,5,4,0,1,-1,2,0,2,2,2,-1,1,4,-1],
+          [-6,-7,-2,-7,-2,-4,0,-7,-8,-6,0,-9,-2,-3,-2,0,-8,-2,-5,-2,-5,-8,-10,0,-2,-9,-9,-7,-1],
+          [-15,2,-11,-14,-15,-5,-5,-18,-18,-19,0,0,2,-7,-16,-7,-4,-1,-1,-16,-18,-10,-3,-19,-6,-16,-16,-8,-2,-18]]
+
+    max_temp = float('-inf')
+    max_temp_kuup = ""
+    
+    for i in range(len(kuud)):
+        for j in range(len(temp[i])):
+            if temp[i][j] > max_temp:
+                max_temp = temp[i][j]
+                max_temp_kuup = f"{j+1} {kuud[i]}"
+    
+    print(f"kuumim kuupäev on: {max_temp_kuup} ja temperatuur sellel päeval oli {max_temp} kraadi")
+Temperatuurid()
 
 
-suurim_igas_kuus = [max(jaanuar), max(veebruar), max(märts), max(aprill), max(mai), max(juuni), max(juuli), max(august), max(september), max(oktoober), max(november), max(detsember)]
-kuud = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"]
-kuupaeva_arvutus_kuu = suurim_igas_kuus.index(max(suurim_igas_kuus)) + 1
-kuupaeva_arvutus_paev = (max(temperatuurid[kuupaeva_arvutus_kuu - 1])) + 1  
+input()
 
-print(f"kõrgeim temeratuur aastas oli {max(suurim_igas_kuus)} ning see oli kuupäeval {kuupaeva_arvutus_paev}.{kuud[kuupaeva_arvutus_kuu - 1] }")
   
 
 
@@ -139,23 +147,26 @@ Eurokalkulaator - koosta programm, mis kalkuleerib valuuta vastavalt kasutaja so
 
 input()
 #7. Ülesanne
-def arvutus(küsimus):
-    #küsib küsimuse
-    küsimus = input("Kas soovid arvutada EUR --> EEK või EEK --> EUR? (EUR --> EEK = 1, EEK --> EUR = 2): ")
-    #kui vastus on 1, siis arvutab eurod kroonideks
-    if küsimus == "1":
-        euro = int(input("Sisesta eurode arv: "))
-        print(f"{euro} eurot on {euro * 15.6466} krooni.")
-        #kui vastus on 2, siis arvutab kroonid eurodeks
-    elif küsimus == "2":
-        kroon = int(input("Sisesta kroonide arv: "))
-        print(f"{kroon} krooni on {kroon / 15.6466} eurot.")
-        #kui vastus on midagi muud, siis ütleb, et sellist valikut ei ole
-    else:
-        print("sellist valikut ei ole! (valige palun kas 1 või 2)")
-print(arvutus("küsimus"))
-input()
+def eurokalkulaator():
+    while True:  
+        print("1. EUR->EEK")
+        print("2. EEK->EUR")
+        print("3. EXIT")
+        valik=int(input("vali: "))
+        if valik==1:
+            summa=int(input("summa: "))
+            print(f"{summa} eurot on {summa*15.6466} krooni")
+        elif valik==2:
+            summa=int(input("summa: "))
+            print(f"{summa} krooni on {summa/15.6466} eurot")
+        elif valik==3:
+            break
+        else:
+            print("vale valik loll lammas")
+eurokalkulaator()
 
+
+input()
 #5. Ülesanne
 
 ostunimekiri = []
